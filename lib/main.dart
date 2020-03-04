17ee255cd5dc6ee2699b122f681c9700aa3f286d
rafay-tariq <rafaytariq369616@gmail.com>
2020-03-04 23:54:49 +0500

    after completing 20 lectures

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
-class MyApp extends StatelessWidget {  
+class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Correct Answer');
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("Helloo ffd f "),
+      home: Scaffold(
+        appBar: AppBar(title: Text("PkAdmissions")),
+        body: Column(
+          children: <Widget>[
+            Text("Answer the Question"),
+            RaisedButton(
+              child: Text("Answer 1"),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text("Answer 2"),
+              onPressed: () {
+                print('Other Answer Choosen');
+              },
+            ),
+            RaisedButton(
+              child: Text("Answer 3"),
+              onPressed: null,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
